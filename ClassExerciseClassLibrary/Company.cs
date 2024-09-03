2c7596b0a4708c48bc3cf8efedb6b063b0a56b89
Jakob Farian Krarup <kr@rup.dk>
2024-09-03 12:17:50 +0200

    Add project files.

---
+++ ClassExerciseClassLibrary/Company.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassExerciseClassLibrary;
+public class Company
+{
+    public string Name { get; set; }
+    public int NumberOfEmployees { get; set; }
+    public bool GovernmentRun { get; set; }
+
+    public Company(string name, int numberOfEmployees = 0, bool governmentRun = false)
+    {
+        Name = name;
+        NumberOfEmployees = numberOfEmployees;
+        GovernmentRun = governmentRun;
+    }
+
+
+    public override string ToString()
+    {
+        return $"This company is named {Name}, has {NumberOfEmployees} employees and is{(GovernmentRun ? " ": " not ")}government run";
+    }
+}
---
+++ ClassExerciseConsoleApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using ClassExerciseClassLibrary;
+using System;
+
+namespace ClassExerciseConsoleApplication;
+class Program
+{
+    private static Company company;
+    static void Main(string[] args)
+    {
+        GetCompanyFromConsoleInput();
+        WriteCompanyInfoAndWaitForKeypress();
+    }
+
+    private static void GetCompanyFromConsoleInput()
+    {
+        Console.Write("Please enter company name: ");
+        string companyName = Console.ReadLine();
+
+        Console.Write("Please enter number of employees: ");
+        string numberOfEmployeesString = Console.ReadLine();
+        if (!int.TryParse(numberOfEmployeesString, out int numberOfEmployees))
+        {
+            Console.WriteLine($"Unable to parse '{numberOfEmployeesString}' to an int. Using default 0");
+        }
+
+        Console.Write("Please enter whether company is government run: (Y/N)");
+        string governmentRunString = Console.ReadLine();
+        bool governmentRun = false;
+        if (!governmentRunString.Contains("n", StringComparison.InvariantCultureIgnoreCase)) { governmentRun = true; }
+        else if (governmentRunString.Contains("n", StringComparison.InvariantCultureIgnoreCase)) { governmentRun = false; }
+        else
+        {
+            Console.WriteLine("Unable to understand input. Government run will be set to false.");
+        }
+        company = new Company(companyName, numberOfEmployees, governmentRun); 
+    }
+
+    private static void WriteCompanyInfoAndWaitForKeypress()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Company created:");
+        Console.WriteLine(company);
+        Console.WriteLine("Any key to exit");
+        Console.ReadKey();
+    }
+}
